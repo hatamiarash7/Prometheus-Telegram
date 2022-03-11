adef46ab9251d37fb1401aa39b80685de2f713a2
Arash Hatami <hatamiarash7@gmail.com>
2022-03-11 13:03:44 +0330

    remove unused model

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,12 +4,3 @@ from pydantic import BaseModel
 class PlainMessageSend(BaseModel):
     chat_id: int
     text: str
-
-
-class Alert(BaseModel):
-    type: str
-    summary: str
-    alert_name: str
-    instance: str
-    serverity: str
-    status: str
