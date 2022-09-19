bc07919d650ec7998fa937a1f3cf461ba7faa58f
Arash Hatami <hatamiarash7@gmail.com>
2022-09-19 15:33:46 +0430

    use legacy method
    Signed-off-by: Arash Hatami <hatamiarash7@gmail.com>

--- app/services/message.py
+++ app/services/message.py
@@ -1,9 +1,16 @@
+import string
 from typing import Optional, NoReturn
 
-from app import schemas
-from app.config import bot_init
+from app import schemas, config
+import requests
 
 
 async def send_message(message: schemas.PlainMessageSend) -> Optional[NoReturn]:
-    async with await bot_init() as bot:
-        await bot.send_message(message.chat_id, message.text)
+    TG_URL = "https://telegram.web-hook.ir/bot%s/sendMessage?parse_mode=MarkdownV2" % config.settings.TG_BOT_TOKEN
+
+    data = {"chat_id": message.chat_id, "text": fixBody(message.text)}
+    requests.post(TG_URL, data)
+
+
+def fixBody(body: string) -> string:
+    return body.replace("[", "\\[").replace("`", "\\`").replace("=", "\\=").replace("-", "\\-")
